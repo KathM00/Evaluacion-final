4c8419f4678b5d3adfbf9beee1fe824109d6c61b
KathM00 <kathymontamejia@gmail.com>
2025-12-19 17:59:01 -0400

    Cambiando DTOS

--- Entities/Dtos/Auth/LoginResponse.cs
+++ Entities/Dtos/Auth/LoginResponse.cs
@@ -17,7 +17,26 @@
         public string Email { get; set; } = string.Empty;
     }
     public class RefreshRequestDto
-    { 
-        public required string RefreshToken {  get; set; }
+    {
+        public required string RefreshToken { get; set; }
     }
+
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; } = string.Empty;
+    }
+
+    public class ResetPasswordDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+
+    public class PasswordResetInfo
+    {
+        public string Email { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+
+
 }
